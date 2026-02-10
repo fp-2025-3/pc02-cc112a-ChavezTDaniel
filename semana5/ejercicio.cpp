c2579235bb96309f34d74502321c08ffdcbf50c8
ChavezTDaniel <daniel74177576@gmail.com>
2026-02-10 17:23:11 -0500

    Add files via upload

--- semana5/ejercicio.cpp
+++ semana5/ejercicio.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+struct estudiantes{
+  int codigo;
+  float promedio;
+};
+
+
+int main(){
+int N;
+cout<<"Ingrese la cantidad de estudiantes: ";
+cin>>N;
+ estudiantes*ptr=new estudiantes[N];
+ 
+for(int i=0;i<N;i++){
+   cout<<"Alumno: "<<i+1<<endl;
+   cout<<"Codigo: ";cin>>(*(ptr+i)).codigo;
+   cout<<"Promedio : ";cin>>(*(ptr+i)).promedio;
+}
+ 
+for(int i=0;i<N;i++){
+ if((*(ptr+i)).promedio>=10){
+   cout<<"Alumnos aprobados: "<<endl;
+   cout<<"Codigo: ";cout<<(*(ptr+i)).codigo<<endl;
+   cout<<"Promedio: ";cout<<(*(ptr+i)).promedio;
+}
+}
+
+ cout<<endl;
+
+delete [] ptr;
+
+    return 0;
+}
+
 
